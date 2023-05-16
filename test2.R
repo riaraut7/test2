2155ba0674d3fd739905440bdc61b45844acbd94
Ria Raut <riaraut7@gmail.com>
2023-05-16 12:51:16 -0700

    added some graphing stuff

--- test2.R
+++ test2.R
@@ -1,6 +1,15 @@
-#edit 1 
-"git add"
-#edit 1 fr
-library(carData)
+#Trying out pushing and pulling code/playing with AT Data 
 
-"hi"
+#radom thing you're trying 
+library(ggplot2)
+library(reshape2)
+library(dplyr)
+
+#Trying to get Data from AT 
+AT <- read.csv("AT_data.csv", stringsAsFactors = TRUE)
+
+library(ggplot2)
+library(dplyr)
+ggplot(data = filter(AT, Taxon %in% c("Plantago lanceloata", "Pinus contorta")), 
+       aes(x= Tleaf_C, y = A_umol_m2_s))+ geom_point() + geom_smooth() + facet_wrap(Taxon~curveID, ncol = 2)
+
